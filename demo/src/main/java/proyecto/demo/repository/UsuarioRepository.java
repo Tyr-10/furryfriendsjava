f075c3910eec03ce41cca7bb285d59b81180be7d
lizet0621 <lizle2727@gmail.com>
2025-06-15 20:59:00 -0500

    cambio codigo

--- demo/src/main/java/proyecto/demo/repository/UsuarioRepository.java
+++ demo/src/main/java/proyecto/demo/repository/UsuarioRepository.java
@@ -1,9 +1,8 @@
 package proyecto.demo.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import proyecto.demo.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByCorreo(String correo);
+    Usuario findByCorreo(String correo); // Correcto
 }
