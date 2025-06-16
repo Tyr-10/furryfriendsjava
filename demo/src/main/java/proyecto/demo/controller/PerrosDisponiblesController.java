84529c998c8fafe578781e399933cb54711a148d
farfancha <emillycinaricicua@gmail.com>
2025-06-15 21:08:13 -0500

    revicion y ediciones

--- demo/src/main/java/proyecto/demo/controller/PerrosDisponiblesController.java
+++ demo/src/main/java/proyecto/demo/controller/PerrosDisponiblesController.java
@@ -23,7 +23,6 @@ public class PerrosDisponiblesController {
             @RequestParam(required = false) String descripcion,
             Model model) {
 
-                
         List<Perros> perros = perrosRepository.findByFiltros(edad, color, tamanio, descripcion);
         model.addAttribute("perros", perros);
         return "perrosdisponibles";
