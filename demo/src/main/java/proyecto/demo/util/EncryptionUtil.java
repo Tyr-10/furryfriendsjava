f6b83d4d0abcf60ddf465a77d15942c5351e4d9a
farfancha <emillycinaricicua@gmail.com>
2025-06-26 23:55:42 -0500

    neuva carpeta, archivo, css y html

---
+++ demo/src/main/java/proyecto/demo/util/EncryptionUtil.java
@@ -0,0 +1,28 @@
+package proyecto.demo.util;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class EncryptionUtil {
+
+    // Llave secreta fija (puedes guardarla segura o generar una con KeyGenerator)
+    private static final String SECRET_KEY = "1234567890123456"; // 16 bytes (128 bits)
+    private static final String ALGORITHM = "AES";
+
+    public static byte[] encrypt(byte[] data) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        return cipher.doFinal(data);
+    }
+
+    public static byte[] decrypt(byte[] encryptedData) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        return cipher.doFinal(encryptedData);
+    }
+}
