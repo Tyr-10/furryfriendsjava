5f93297dd286a48dafdb20e4d1c8369a0c188b04
farfancha <emillycinaricicua@gmail.com>
2025-06-26 22:48:56 -0500

    agregaciones y correcciones

---
+++ demo/src/main/java/proyecto/demo/model/Seguimiento.java
@@ -0,0 +1,33 @@
+package proyecto.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "seguimiento_visitas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Seguimiento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String archivo;
+
+    @Column(name = "nombre_original")
+    private String nombreOriginal;
+
+    @Column(name = "rol_id")
+    private Long rolId;
+
+    @Column(name = "usuario_id")
+    private Long usuarioId;
+
+    private Boolean activo;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", insertable = false, updatable = false)
+    private Usuario usuario;
+}
